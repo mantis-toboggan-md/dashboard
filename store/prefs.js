b438005eba482e487ae24ce1aaabd0271c5ef645
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2021-04-21 14:01:21 -0700

    suse dark theme stylesheet;

--- store/prefs.js
+++ store/prefs.js
@@ -258,7 +258,7 @@ export const actions = {
 
   async setTheme({ dispatch }, val) {
     await dispatch('set', { key: THEME, value: val });
-    dispatch('setBrandStyle', val === 'dark');
+    // dispatch('setBrandStyle', val === 'dark');
   },
 
   loadCookies({ state, commit }) {
@@ -328,7 +328,7 @@ export const actions = {
     function changed(value) {
       // console.log('Prefers Theme:', value);
       dispatch('set', { key: PREFERS_SCHEME, value });
-      dispatch('setBrandStyle', value);
+      // dispatch('setBrandStyle', value);
     }
 
     function fromClock() {
