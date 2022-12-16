da156c8d41f9cec6f2536ba83d7bff491d91b7a6
Catherine Luse <catherine.luse@gmail.com>
2022-12-16 01:57:24 -0700

    Change Azure VM size endpoint

--- shell/machine-config/azure.vue
+++ shell/machine-config/azure.vue
@@ -161,7 +161,7 @@ export default {
       }
 
       this.vmSizes = await this.$store.dispatch('management/request', {
-        url: addParams('/meta/aksVMSizes', {
+        url: addParams('/meta/aksVMSizesV2', {
           cloudCredentialId: this.credentialId,
           region:            this.value.location
         }),
@@ -355,7 +355,7 @@ export default {
       // VMs are supported in different regions.
       try {
         this.vmSizes = await this.$store.dispatch('management/request', {
-          url: addParams('/meta/aksVMSizes', {
+          url: addParams('/meta/aksVMSizesV2', {
             cloudCredentialId: this.credentialId,
             region:            this.value.location
           }),
