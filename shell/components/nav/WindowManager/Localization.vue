e25f6567da0017cf858cae67d48f4df56c4db17a
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-01-31 13:55:54 -0700

    wm component

---
+++ shell/components/nav/WindowManager/Localization.vue
@@ -0,0 +1,25 @@
+<script>
+import Window from './';
+export default {
+  components: { Window },
+
+  props: {
+    active: {
+      type:    Boolean,
+      default: false
+    }
+  }
+
+};
+</script>
+
+<template>
+  <Window>
+    <template #body>
+      <div>wow_its_fucking_nothing.jpeg</div>
+    </template>
+    <template #title>
+      download button go here
+    </template>
+  </Window>
+</template>
