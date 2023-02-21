8353e4f4a799bf8f5c3698e4aa4f554aa3360b25
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2023-02-21 15:16:22 -0700

    use list of schemas

--- shell/edit/resourceview/index.vue
+++ shell/edit/resourceview/index.vue
@@ -5,8 +5,9 @@ import LabeledSelect from '@shell/components/form/LabeledSelect';
 import { ToggleSwitch } from '@components/Form/ToggleSwitch';
 import { LabeledInput } from '@components/Form/LabeledInput';
 import TablePreview from './TablePreview';
-import { MANAGEMENT, CUSTOM_RESOURCE_DEFINITION } from '@shell/config/types';
+import { MANAGEMENT, CUSTOM_RESOURCE_DEFINITION, SCHEMA } from '@shell/config/types';
 import { fetchOrCreateSetting } from '@shell/utils/settings';
+import { mapGetters } from 'vuex';
 
 /*
 A resourceview is a spoofed type saved in a global setting 'resourceviews'
@@ -51,18 +52,23 @@ export default {
       cluster:             null,
       // all customresourcedefinition objects, fetched when a cluster is selected
       crds:                [],
-      hideSystemResources: true,
-      targetCrd:           this.value?.id,
+      schemas:             [],
+      hideSystemResources: false,
+      // targetCrd:           this.value?.id,
+      targetSchema:        this.value?.id,
       targetPath:          null,
       targetLabel:         null,
       columns:             this.value?.columns || [],
-      loadingCrds:         false,
+      // loadingCrds:         false,
+      loadingSchemas:      false,
     };
   },
 
   methods: {
     save(btnCB) {
-      this.$set(this.parsedSetting, this.targetCrd, { id: this.targetCrd, columns: this.columns });
+      // this.$set(this.parsedSetting, this.targetCrd, { id: this.targetCrd, columns: this.columns });
+      this.$set(this.parsedSetting, this.targetSchema, { id: this.targetSchema, columns: this.columns });
+
       try {
         this.viewSetting.value = JSON.stringify(this.parsedSetting);
       } catch (e) {
@@ -83,36 +89,72 @@ export default {
       this.targetPath = null;
     },
 
-    fetchCrds() {
-      this.loadingCrds = true;
-      const url = `/k8s/clusters/${ this.cluster }/v1/${ CUSTOM_RESOURCE_DEFINITION }s`;
+    // fetchCrds() {
+    //   this.loadingCrds = true;
+    //   const url = `/k8s/clusters/${ this.cluster }/v1/${ CUSTOM_RESOURCE_DEFINITION }s`;
+
+    //   this.$store.dispatch('management/request', { opt: { url } }).then((res) => {
+    //     const { data = [] } = res;
+
+    //     this.crds = data;
+    //     this.loadingCrds = false;
+    //   }).catch((err) => {
+    //     // TODO nb what to do with cluster loading error?
+    //     console.error(err);
+    //   });
+    // },
+
+    fetchSchemas() {
+      this.loadingSchemas = true;
+      const url = `/k8s/clusters/${ this.cluster }/v1/${ SCHEMA }s`;
 
       this.$store.dispatch('management/request', { opt: { url } }).then((res) => {
         const { data = [] } = res;
 
-        this.crds = data;
-        this.loadingCrds = false;
+        this.schemas = data;
+        this.loadingSchemas = false;
       }).catch((err) => {
         // TODO nb what to do with cluster loading error?
         console.error(err);
       });
     },
 
-    isSystemCrd(crd) {
-      // TODO nb use constant, also is this only management group?
-      return crd.group === 'management.cattle.io';
+    // isSystemCrd(crd) {
+    //   // TODO nb use constant, also is this only management group?
+    //   return crd.group === 'management.cattle.io';
+    // },
+
+    // Just going to assume that all resources have metadata - any schema without must be defining something within a resource
+    isRootSchema(schema) {
+      return schema.resourceFields.metadata;
+    },
+
+    // TODO nb what schemas should be hidden?
+    isSystemSchema(schema) {
+      return false;
+    },
+
+    // TODO nb filter out resources with entirely custom list views, eg namespaces
+    hasCustomListView(schema) {
+      return false;
+    },
+
+    hideSchema(schema) {
+      return !this.isRootSchema(schema) && !this.hasCustomListView(schema) && (!this.isSystemSchema(schema) || !this.hideSystemResources);
     }
   },
 
   watch: {
     cluster(neu) {
       if (neu) {
-        this.fetchCrds();
+        // this.fetchCrds();
+        this.fetchSchemas();
       }
     }
   },
 
   computed: {
+    ...mapGetters({ headersFor: 'type-map/headersFor' }),
     // parsedSetting: {
     //   get() {
     //     try {
@@ -154,10 +196,20 @@ export default {
 
     // },
 
-    crdOpts() {
-      return this.crds.reduce((opts, crd) => {
-        if (!this.isSystemCrd(crd)) {
-          opts.push({ label: crd?.spec?.names?.kind || crd.id, value: crd.id });
+    // crdOpts() {
+    //   return this.crds.reduce((opts, crd) => {
+    //     if (!this.isSystemCrd(crd)) {
+    //       opts.push({ label: crd.spec.names.plural || crd.id, value: crd });
+    //     }
+
+    //     return opts;
+    //   }, []);
+    // },
+
+    schemaOpts() {
+      return this.schemas.reduce((opts, schema) => {
+        if (!this.hideSchema(schema)) {
+          opts.push({ label: schema.id, value: schema });
         }
 
         return opts;
@@ -171,7 +223,12 @@ export default {
           value: c.id
         };
       });
+    },
+
+    headersFromSchema() {
+      return this.targetSchema ? this.headersFor(this.targetSchema) : [];
     }
+
   }
 };
 </script>
@@ -210,13 +267,23 @@ export default {
           />
         </div>
         <div class="col span-5">
-          <!-- TODO nb translation -->
+          <!-- TODO nb translation
           <LabeledSelect
             v-model="targetCrd"
             :options="crdOpts"
             :mode="mode"
             label="Custom Resource Definition"
             :loading="loadingCrds"
+            taggable
+            searchable
+          /> -->
+          <!-- TODO nb translation -->
+          <LabeledSelect
+            v-model="targetSchema"
+            :options="schemaOpts"
+            :mode="mode"
+            label="Resource Type"
+            :loading="loadingSchemas"
             taggable
             searchable
           />
@@ -230,43 +297,57 @@ export default {
         </div>
       </div>
 
-      <!-- //TODO nb avoid inline style  -->
-      <!-- :style="{'flex-direction': 'row-reverse'}" -->
-      <div
-        class="row mb-10"
-      >
-        <div class="col span-5">
-          <LabeledInput
-            v-model="targetPath"
-            :mode="mode"
-            label="Path to Display Value"
-            placeholder="e.g. metadata.namespace"
-            tooltip="The path can be any valid JSONPath, e.g. '$['spec']['rules'][0].host'"
-          />
+      <template v-if="targetSchema">
+        <div
+          v-for="col in headersFromSchema"
+          :key="col.name"
+        >
+          <ul>
+            <li
+              v-for="(val, key) in col"
+              :key="key"
+            >
+              {{ key }}:{{ val }}
+            </li>
+          </ul>
         </div>
-        <div class="col span-5">
-          <LabeledInput
-            v-model="targetLabel"
-            :mode="mode"
-            label="Column Label"
-            placeholder="e.g. Namespace"
-          />
+        <div
+          class="row mb-10"
+        >
+          <div class="col span-5">
+            <LabeledInput
+              v-model="targetPath"
+              :mode="mode"
+              label="Path to Display Value"
+              placeholder="e.g. metadata.namespace"
+              tooltip="The path can be any valid JSONPath, e.g. '$['spec']['rules'][0].host'"
+            />
+          </div>
+          <div class="col span-5">
+            <LabeledInput
+              v-model="targetLabel"
+              :mode="mode"
+              label="Column Label"
+              placeholder="e.g. Namespace"
+            />
+          </div>
+          <div class="col span-2">
+            <button
+              type="button"
+              class="btn role-primary"
+              :disabled="!targetPath || !targetLabel"
+              @click="addColumn"
+            >
+              <!-- TODO nb translation -->
+              Add Column
+            </button>
+          </div>
         </div>
-        <div class="col span-2">
-          <button
-            type="button"
-            class="btn role-primary"
-            @click="addColumn"
-          >
-            <!-- TODO nb translation -->
-            Add Column
-          </button>
-        </div>
-      </div>
 
-      <div class="row mb-10">
-        <TablePreview :columns="columns" />
-      </div>
+        <div class="row mb-10">
+          <TablePreview :columns="columns" />
+        </div>
+      </template>
     </CruResource>
   </div>
 </template>
