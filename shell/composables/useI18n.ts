39ab13cb4a1ff5a6f95adcdd4e66c08a42cfdbe3
Phillip Rak <rak.phillip@gmail.com>
2024-06-27 09:16:39 -0700

    Add translations for the user retention growl
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ shell/composables/useI18n.ts
@@ -0,0 +1,15 @@
+import { stringFor } from '@shell/plugins/i18n';
+import { useStore } from '@shell/composables/useStore';
+
+/**
+ * Allows for consuming i18n strings with the Vue composition API.
+ * @param key - The key for the i18n string to translate.
+ * @param args - An object or array containing arguments for the translation function.
+ * @param raw - A boolean determining if the string returned is a raw representation.
+ * @returns A translated string or the raw value if the raw parameter is set to true.
+ */
+export const t = (key: string, args: unknown, raw: boolean): unknown => {
+  const store = useStore();
+
+  return stringFor(store, key, args, raw);
+};
--- shell/pages/c/_cluster/auth/user.retention/index.vue
+++ shell/pages/c/_cluster/auth/user.retention/index.vue
@@ -5,6 +5,7 @@ import { useRouter, onBeforeRouteUpdate } from 'vue-router/composables';
 import UserRetentionHeader from '@shell/components/user.retention/user-retention-header.vue';
 import Footer from '@shell/components/form/Footer.vue';
 import { useStore } from '@shell/composables/useStore';
+import { t } from '@shell/composables/useTranslation';
 import { MANAGEMENT } from '@shell/config/types';
 import { SETTING } from '@shell/config/settings';
 import { isAdminUser } from '@shell/store/type-map';
@@ -216,8 +217,8 @@ const save = async(btnCB: (arg: boolean) => void) => {
     store.dispatch(
       'growl/success',
       {
-        title:   'Save user retention settings',
-        message: 'User retention settings have been updated successfully',
+        title:   t('user.retention.growl.title'),
+        message: t('user.retention.growl.message'),
       },
       { root: true }
     );
