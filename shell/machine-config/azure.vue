ab6409f39ce0621605c3f15060cf478fda521c40
Phillip Rak <rak.phillip@gmail.com>
2026-02-18 15:31:15 -0700

    Add a prominent warning when the mode is edit
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- shell/machine-config/azure.vue
+++ shell/machine-config/azure.vue
@@ -518,6 +518,11 @@ export default {
     </div>
   </div>
   <div v-else>
+    <Banner
+      v-if="mode === _EDIT && !value.managedDisks"
+      color="warning"
+      :label="t('cluster.machineConfig.azure.managedDisks.deprecationWarning', {}, true)"
+    />
     <div class="row mt-20">
       <div class="col span-6">
         <LabeledSelect
