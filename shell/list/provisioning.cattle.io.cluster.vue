403fc78649a5dd4df8105f3c16fcb8106e4d31bd
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2026-02-19 10:28:15 -0800

    block editing v2 prov capi clusters

--- shell/list/provisioning.cattle.io.cluster.vue
+++ shell/list/provisioning.cattle.io.cluster.vue
@@ -167,6 +167,14 @@ export default {
 
   },
 
+  methods: {
+    getCustomDetailLink(cluster) {
+      console.log('*** getCustomDetailLink', cluster);
+
+      return cluster.isCapiHybrid ? null : cluster.detailLocation;
+    }
+  },
+
   $loadingResources() {
     // results are filtered so we wouldn't get the correct count on indicator...
     return { loadIndeterminate: true };
@@ -211,7 +219,6 @@ export default {
         </router-link>
       </template>
     </Masthead>
-
     <ResourceTable
       :headers="headers"
       :table-actions="true"
@@ -221,6 +228,7 @@ export default {
       :use-query-params-for-simple-filtering="useQueryParamsForSimpleFiltering"
       :data-testid="'cluster-list'"
       :force-update-live-and-delayed="forceUpdateLiveAndDelayed"
+      :get-custom-detail-link="getCustomDetailLink"
     >
       <!-- Why are state column and subrow overwritten here? -->
       <!-- for rke1 clusters, where they try to use the mgmt cluster stateObj instead of prov cluster stateObj,  -->
